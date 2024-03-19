31766ef17f5e4cf8fcb677c7bddeebf67448a4fa
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-03-19 19:20:02 +0530

    Todo and tocontext type defined

--- src/store/todos.tsx
+++ src/store/todos.tsx
@@ -1,11 +1,26 @@
-import { createContext } from "react";
+import { ReactNode, createContext } from "react";
+
+export type TodosProviderProps = {
+    children: ReactNode
+}
+
+export type Todo = {
+    id: string;
+    task:string;
+    completed:boolean;
+    createdAt:Date;
+
+}
+
+export type TodosContext = {
+    todos:[];
+}
+
+export const todoContext = createContext(null)
 
 
-export const todoContext =  createContext(null)
-
-
-export const TodosProvider = ({children}) => {
-    return <todoContext.Provider>
+export const TodosProvider = ({children}: TodosProviderProps) => {
+    return <todoContext.Provider value={{}}>
         {children}
     </todoContext.Provider>
 }
