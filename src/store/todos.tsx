b7637c6def24e7989e48d40880733398bb6fd92b
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-03-23 23:52:27 +0530

    bug fixed

--- src/store/todos.tsx
+++ src/store/todos.tsx
@@ -1,27 +1,27 @@
 import { ReactNode, createContext } from "react";
 
 export type TodosProviderProps = {
-    children: ReactNode
-}
+  children: ReactNode;
+};
 
 export type Todo = {
-    id: string;
-    task:string;
-    completed:boolean;
-    createdAt:Date;
-
-}
+  id: string;
+  task: string;
+  completed: boolean;
+  createdAt: Date;
+};
 
 export type TodosContext = {
-    todos:[];
-    handleAddTodDo:(task:string) => void
-}
+  todos: [];
+  handleAddTodDo: (task: string) => void;
+};
 
-export const todoContext = createContext<TodosContext>(null)
+export const todoContext = createContext<TodosContext>(null);
 
-
-export const TodosProvider = ({children}: TodosProviderProps) => {
-    return <todoContext.Provider value={{todo, handleAddTodDo}}>
-        {children}
+export const TodosProvider = ({ children }: TodosProviderProps) => {
+  return (
+    <todoContext.Provider value={{ todo, handleAddTodDo }}>
+      {children}
     </todoContext.Provider>
-}
+  );
+};
