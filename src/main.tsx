fe97c8baf196f18d6f93c462e231a6af973802f8
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-03-27 16:07:51 +0530

    wrapped with browser router

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { TodosProvider } from './store/todos.tsx'
+import { TodosProvideer } from './store/todos.tsx'
+import {BrowserRouter} from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <TodosProvider>
-    <App />
-    </TodosProvider>
+    <BrowserRouter>
+    <TodosProvideer>
+     <App />
+    </TodosProvideer>
+    </BrowserRouter>
   </React.StrictMode>,
 )
