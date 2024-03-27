4246b83cdf16c8d760fb88c40778b8b85e065847
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-03-27 16:08:29 +0530

    final commit done

--- src/components/addtodo.tsx
+++ src/components/addtodo.tsx
@@ -1,23 +1,26 @@
-import { FormEvent, useState } from "react"
-import { useTodos } from "../store/todos"
+import { FormEvent, useState } from "react";
+import { useTodos } from "../store/todos";
 
-const Addtodo = () => {
-    const [todo, setTodo] = useState("")
-    const {handleAddTodDo} = useTodos();
+const AddToDo = () => {
+  const [todo, setTodo] = useState("");
+  const { handleAddToDo } = useTodos();
 
-    const handleFormSubmit = (e:FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        handleAddTodDo(todo)
-        setTodo("")
-    }
-
-    
+  const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    handleAddToDo(todo);
+    setTodo("");
+  };
 
   return (
-    <form onSubmit={handleFormSubmit} >
-        <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
-        <button type="submit">ADD</button>
+    <form onSubmit={handleFormSubmit}>
+      <input
+        type="text"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button type="submit">Add</button>
     </form>
-  )
-}
-export default Addtodo
+  );
+};
+
+export default AddToDo;
