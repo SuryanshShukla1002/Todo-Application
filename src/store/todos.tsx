82a2a2b99a5c70416be2c72507d48a27687d4d45
shuklarajni745@gmail.com <shuklarajni745@gmail.com>
2024-03-28 22:41:21 +0530

    handleDelettode done

--- src/store/todos.tsx
+++ src/store/todos.tsx
@@ -12,17 +12,25 @@ export type Todo = {
 };
 
 export type TodosContext = {
-  todos:Todo[];
-  handleAddTodDo:(task:string) => void;
-  toggleTodoAsCompleted
+  todos: Todo[];
+  handleAddToDo: (task: string) => void; // call signature
+  toggleTodoAsCompleted: (id: string) => void;
+  handleDeleteTodo: (id: string) => void;
 };
 
-export const todoContext = createContext<TodosContext | null>(null);
+export const todosContext = createContext<TodosContext | null>(null);
 
-export const TodosProvider = ({ children }: TodosProviderProps) => {
-  const [todos, setTodos] = useState<Todo[]>([]);
+export const TodosProvideer = ({ children }: TodosProviderProps) => {
+  const [todos, setTodos] = useState<Todo[]>(() => {
+    try {
+      const newTodos = localStorage.getItem("todos") || "[]";
+      return JSON.parse(newTodos) as Todo[];
+    } catch (error) {
+      return [];
+    }
+  });
 
-  const handleAddTodDo = (task: string) => {
+  const handleAddToDo = (task: string) => {
     setTodos((prev) => {
       const newTodos: Todo[] = [
         {
@@ -33,37 +41,49 @@ export const TodosProvider = ({ children }: TodosProviderProps) => {
         },
         ...prev,
       ];
+      //   console.log("my previous " + prev);
+      //   console.log(newTodos);
+      localStorage.setItem("todos", JSON.stringify(newTodos));
       return newTodos;
     });
   };
 
-
-  const toggleTodoAsCompleted = (id:string) => {
+  // mark compelted
+  const toggleTodoAsCompleted = (id: string) => {
     setTodos((prev) => {
       let newTodos = prev.map((todo) => {
-        if(todo.id === id){
-          return {...todo, completed:!todo.completed}
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
         }
-      })
-      return newTodos
-    })
-  }
+        return todo;
+      });
+      localStorage.setItem("todos", JSON.stringify(newTodos));
+      return newTodos;
+    });
+  };
 
-
-
+  const handleDeleteTodo = (id: string) => {
+    setTodos((prev) => {
+      let newTodos = prev.filter((filterTodo) => filterTodo.id !== id);
+      localStorage.setItem("todos", JSON.stringify(newTodos));
+      return newTodos;
+    });
+  };
 
   return (
-    <todoContext.Provider value={{ todos, handleAddTodDo }}>
+    <todosContext.Provider
+      value={{ todos, handleAddToDo, toggleTodoAsCompleted, handleDeleteTodo }}
+    >
       {children}
-    </todoContext.Provider>
+    </todosContext.Provider>
   );
 };
 
 // consumer
 export const useTodos = () => {
-  const todosConsumer = useContext(todoContext);
-  if (!todoContext) {
-    throw new Error("useTodo used outside the provider");
+  const todosConsumer = useContext(todosContext);
+  if (!todosConsumer) {
+    throw new Error("useTodos used outside of Provider");
   }
   return todosConsumer;
 };
